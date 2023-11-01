f660d6e74e2fe33f882864eefae2bbe1ba55ca12
Arish V <arishnarayanan121@gmail.com>
2023-11-01 22:08:25 +0530

    blog-push

--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// // const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = nextConfig
+// module.exports =  {
+//       env: {
+//         mongodb_username: 'arishv',
+//         mongodb_password: 'DkZz90qMZovL8mWJ',
+//         mongodb_clustername: 'cluster0',
+//         mongodb_database: 'my-site',
+//       },
+//     };
+  
+ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: 'arishv',
+        mongodb_password: 'DkZz90qMZovL8mWJ',
+        mongodb_clustername: 'cluster0',
+        mongodb_database: 'my-site-dev',
+      },
+    };
+  }
+
+  return {
+    env: {
+      mongodb_username: 'arishv',
+      mongodb_password: 'DkZz90qMZovL8mWJ',
+      mongodb_clustername: 'cluster0',
+      mongodb_database: 'my-site',
+    },
+  };
+};
